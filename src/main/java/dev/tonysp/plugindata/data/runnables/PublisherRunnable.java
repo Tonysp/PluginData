5b23d330617827d8bd3e62137465c726a3f78850
Tonysp <tonysyspplz@gmail.com>
2020-10-19 00:48:01 +0200

    - added delay before starting publisher thread to avoid unexpected behavior - renamed DataPacketReceivePubSubEvent

--- src/main/java/dev/tonysp/plugindata/data/runnables/PublisherRunnable.java
+++ src/main/java/dev/tonysp/plugindata/data/runnables/PublisherRunnable.java
@@ -22,7 +22,7 @@ public class PublisherRunnable implements Runnable {
     public void run() {
         try (Jedis jedis = jedisPool.getResource()) {
             jedis.auth(redisPassword);
-            Thread.sleep(500);
+            Thread.sleep(2000);
 
             while (true) {
                 DataPacket message = ((LinkedBlockingQueue<DataPacket>) PubSubPipelineManager.getInstance().getReadyToSend()).take();
